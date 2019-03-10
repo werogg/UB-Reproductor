69363e036a098c0292321e8df0f52a98d84996ff
Joel Otero <supwer00@gmail.com>
2019-03-10 17:06:20 +0100

    ControlMenu implemented directly in AplicacioUB1. ControlMenu.java deleted.

--- src/edu/ub/prog2/VinagreJorgeOteroJoel/vista/AplicacioUB1.java
+++ src/edu/ub/prog2/VinagreJorgeOteroJoel/vista/AplicacioUB1.java
@@ -7,6 +7,7 @@ package edu.ub.prog2.VinagreJorgeOteroJoel.vista;
 
 import edu.ub.prog2.VinagreJorgeOteroJoel.model.CarpetaFitxers;
 import edu.ub.prog2.VinagreJorgeOteroJoel.model.FitxerMultimedia;
+import edu.ub.prog2.utils.Menu;
 import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -21,18 +22,58 @@ public class AplicacioUB1 {
     private final CarpetaFitxers carpeta; 
     private FitxerMultimedia fm;
     
+    // Declarem les opcions per a referir-se a les opcions del menÃº.
+    static private enum OpcionsMenuPrincipal {PRINT_FOLDER,ADD_FILE,DEL_FILE,EXIT};
+    static private enum OpcionsSubmenu1 {MENU_S1_OPCIO1,MENU_S1_OPCIO2,MENU_S1_SORTIR};
+    // Declarem descripcions personalitzades per a les opcions del menÃº principal
+    static private String[] descMenuPrincipal={"Mostrar carpeta",
+                                               "Afegir arxiu",
+                                               "Eliminar arxiu",
+                                               "Sortir"};
+    
     AplicacioUB1() {
         carpeta = new CarpetaFitxers();
+    }
+    
+    public void manager(Scanner sc) {
+
+        // Creem l'objecte per al menÃº. Li passem com a primer parÃ metre el nom del menÃº
+        Menu<OpcionsMenuPrincipal> menu=new Menu<OpcionsMenuPrincipal>("Menu Principal",OpcionsMenuPrincipal.values());
+
+        // Assignem la descripciÃ³ de les opcions
+        menu.setDescripcions(descMenuPrincipal);
+
+        // Obtenim una opciÃ³ des del menÃº i fem les accions pertinents
+        OpcionsMenuPrincipal opcio = null;
+        do {
+            // Mostrem les opcions del menÃº
+            menu.mostrarMenu();
+            // Demanem una opcio
+            opcio=menu.getOpcio(sc);
+
+            // Fem les accions necessÃ ries
+            switch(opcio) {
+                case PRINT_FOLDER:
+                    System.out.println(carpeta);
+                    break;
+                case ADD_FILE:
+                    this.addFileOption();
+                    break;
+                case DEL_FILE:
+                    this.removeFileOption();
+                    break;
+                case EXIT:
+                    System.out.println("Fins aviat!");
+                    break;
+            }
+
+        } while(opcio!=OpcionsMenuPrincipal.EXIT);
     }
     
     public void gestioAplicacioUB() {
         Scanner input = new Scanner(System.in);
         
-        ControlMenu app = new ControlMenu(this);
-        
-        app.manager(input);
-        
-        
+        manager(input);
     }
     
     public CarpetaFitxers getCarpeta() {
