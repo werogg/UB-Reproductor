51b8cfa135927006cdd3bf84072401eb52822ff1
joteroma7.alumnes <joteroma7.alumnes@ub052072.mat.ub.edu>
2019-03-14 13:19:48 +0100

    Controller implemented. ("Controlador.java")

--- src/edu/ub/prog2/VinagreJorgeOteroJoel/vista/AplicacioUB2.java
+++ src/edu/ub/prog2/VinagreJorgeOteroJoel/vista/AplicacioUB2.java
@@ -1,37 +1,29 @@
 package edu.ub.prog2.VinagreJorgeOteroJoel.vista;
 
 import edu.ub.prog2.VinagreJorgeOteroJoel.controlador.Controlador;
-import edu.ub.prog2.VinagreJorgeOteroJoel.model.CarpetaFitxers;
-import edu.ub.prog2.VinagreJorgeOteroJoel.model.Dades;
-import edu.ub.prog2.VinagreJorgeOteroJoel.model.FitxerMultimedia;
 import edu.ub.prog2.utils.Menu;
-import java.io.FileNotFoundException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-import javax.naming.LimitExceededException;
 
 public class AplicacioUB2 {
     
-    private Dades data;
-    private Controlador controlador;
-    private final CarpetaFitxers carpeta; 
-    private FitxerMultimedia fm;
+    private final Controlador controlador;
     
     // Declarem les opcions per a referir-se a les opcions del menÃº.
     static private enum OpcionsMenuPrincipal {PRINT_FOLDER,ADD_FILE,DEL_FILE,EXIT};
     static private enum OpcionsSubmenu1 {MENU_S1_OPCIO1,MENU_S1_OPCIO2,MENU_S1_SORTIR};
     // Declarem descripcions personalitzades per a les opcions del menÃº principal
-    static private String[] descMenuPrincipal={"Mostrar carpeta",
-                                               "Afegir arxiu",
-                                               "Eliminar arxiu",
-                                               "Sortir"};
+    
+    private static final String[] MENU_DESC = {
+        "Mostrar carpeta",
+        "Afegir arxiu",
+        "Eliminar arxiu",
+        "Sortir"
+    };
     
     /**
     * Constructor for class AplicacioUB1
     */
     AplicacioUB2() {
-        carpeta = new CarpetaFitxers();
-        data = new Dades();
         controlador = new Controlador();
     }
     
@@ -42,13 +34,14 @@ public class AplicacioUB2 {
     public void manager(Scanner sc) {
 
         // Creem l'objecte per al menÃº. Li passem com a primer parÃ metre el nom del menÃº
-        Menu<OpcionsMenuPrincipal> menu=new Menu<OpcionsMenuPrincipal>("Menu Principal",OpcionsMenuPrincipal.values());
+        Menu<OpcionsMenuPrincipal> menu=new Menu<>("Menu Principal",OpcionsMenuPrincipal.values());
 
         // Assignem la descripciÃ³ de les opcions
-        menu.setDescripcions(descMenuPrincipal);
+        menu.setDescripcions(MENU_DESC);
 
         // Obtenim una opciÃ³ des del menÃº i fem les accions pertinents
-        OpcionsMenuPrincipal opcio = null;
+        OpcionsMenuPrincipal opcio;
+        
         do {
             // Mostrem les opcions del menÃº
             menu.mostrarMenu();
@@ -58,7 +51,7 @@ public class AplicacioUB2 {
             // Fem les accions necessÃ ries
             switch(opcio) {
                 case PRINT_FOLDER:
-                    System.out.println(carpeta);
+                    System.out.println(controlador.getFolder());
                     break;
                 case ADD_FILE:
                     this.addFileOption();
@@ -84,13 +77,6 @@ public class AplicacioUB2 {
     }
     
     /**
-    * @return A CarpetaFitxers object (The folder)
-    */
-    public CarpetaFitxers getCarpeta() {
-        return carpeta;
-    }
-    
-    /**
     * Option to add a new file to the folder
     */
     private void addFileOption() {
@@ -104,14 +90,7 @@ public class AplicacioUB2 {
         System.out.println("Introdueix la descripció del teu fitxer:");
         String nou_desc = sc.nextLine();
         
-        fm = new FitxerMultimedia(nou_cami);
-        fm.setDescripcio(nou_desc);
-        
-        try {
-            carpeta.addFitxer(fm);
-        } catch (LimitExceededException e) {
-            System.err.println(e.getCause());
-        } 
+        controlador.FolderAddFile(nou_cami, nou_desc);
     }
     
     /**
@@ -119,21 +98,10 @@ public class AplicacioUB2 {
     */
     private void removeFileOption() {
         Scanner sc = new Scanner(System.in);
-        int index_arxiu_sel = -1;
         
         System.out.println("Quin arxiu vols eliminar? [Index]");
+        int index_arxiu_sel = sc.nextInt();
         
-        try {
-            index_arxiu_sel = sc.nextInt();
-        } catch (InputMismatchException e) {
-            
-        }
-        
-        try {
-            fm = (FitxerMultimedia) carpeta.getAt(--index_arxiu_sel);
-            carpeta.removeFitxer(fm);
-        } catch (FileNotFoundException | IndexOutOfBoundsException e) {
-            System.err.println(e.getMessage());
-        }
+        controlador.FolderRemoveFile(index_arxiu_sel);
     }
 }
