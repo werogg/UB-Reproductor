c642838f6a5341b4847d83849caa7d4d1c9a3cb4
JORGEC <JORGEC@jorgecportatil>
2019-03-14 12:37:12 +0100

    Class BiobliotecaFitxersMultimedia added.

---
+++ src/edu/ub/prog2/VinagreJorgeOteroJoel/model/BibliotecaFitxersMultimedia.java
@@ -0,0 +1,9 @@
+package edu.ub.prog2.VinagreJorgeOteroJoel.model;
+/**
+ *
+ * @author Jorge Vinagre and Joel Otero
+ */
+public class BibliotecaFitxersMultimedia extends CarpetaFitxers {
+    
+    
+}
