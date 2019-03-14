c7f0c78f806ee034936bdae1e38bdde7136f54e1
Joel Otero <supwer00@gmail.com>
2019-03-14 12:49:56 +0100

    Revert "Liurament2"

--- src/edu/ub/prog2/VinagreJorgeOteroJoel/controlador/Controlador.java
+++
@@ -1,5 +0,0 @@
-package edu.ub.prog2.VinagreJorgeOteroJoel.controlador;
-
-public class Controlador {
-    
-}
--- src/edu/ub/prog2/VinagreJorgeOteroJoel/model/BibliotecaFitxersMultimedia.java
+++
@@ -1,9 +0,0 @@
-package edu.ub.prog2.VinagreJorgeOteroJoel.model;
-/**
- *
- * @author Jorge Vinagre and Joel Otero
- */
-public class BibliotecaFitxersMultimedia extends CarpetaFitxers {
-    
-    
-}
