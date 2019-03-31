515d0c133f2b1a0a33e61d0b98c181fdf7a1067c
JORGEC <JORGEC@JORGEC-PC.Home>
2019-03-31 14:32:05 +0200

    Frase de BibliotecaFitxersMultimedia corregida. Clase Reproductor creada.

---
+++ src/edu/ub/prog2/VinagreJorgeOteroJoel/model/Reproductor.java
@@ -0,0 +1,13 @@
+package edu.ub.prog2.VinagreJorgeOteroJoel.model;
+
+import edu.ub.prog2.utils.ReproductorBasic;
+
+/**
+ *
+ * @author JORGEC
+ */
+public class Reproductor extends ReproductorBasic {
+    
+    
+    
+}
