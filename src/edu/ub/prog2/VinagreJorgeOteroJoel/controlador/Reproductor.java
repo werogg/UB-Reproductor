a0c7e8682ba392f9763e7af6dcf6bbdbe470eb10
Joel Otero <supwer00@gmail.com>
2019-04-23 14:29:24 +0200

    Play methods implemented in Reproductor

--- src/edu/ub/prog2/VinagreJorgeOteroJoel/controlador/Reproductor.java
+++ src/edu/ub/prog2/VinagreJorgeOteroJoel/controlador/Reproductor.java
@@ -1,11 +1,24 @@
 package edu.ub.prog2.VinagreJorgeOteroJoel.controlador;
 
+import edu.ub.prog2.VinagreJorgeOteroJoel.model.Audio;
+import edu.ub.prog2.VinagreJorgeOteroJoel.model.FitxerReproduible;
+import edu.ub.prog2.utils.AplicacioException;
 import edu.ub.prog2.utils.EscoltadorReproduccioBasic;
 import edu.ub.prog2.utils.ReproductorBasic;
+import java.io.File;
 
 public class Reproductor extends ReproductorBasic {
 
     public Reproductor(EscoltadorReproduccioBasic controlador) {
         super(controlador);
+    }
+    
+    public void reprodueix(FitxerReproduible fr) throws AplicacioException {
+        this.play(fr);
+    }
+    
+    public void reprodueix(Audio audio, File file) throws AplicacioException {
+        this.play(audio, file);
+        this.show(file, (int) audio.getDurada());
     }
 }
